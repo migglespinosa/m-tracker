ef5d9b97256f3547bdcd78cf9b69e497eb620c1e
Miguel Espinosa <miguelespinosaiv@gmail.com>
2021-07-26 23:29:34 -0400

    Refactored server/config.js

--- server/config.js
+++ server/config.js
@@ -1,18 +1,13 @@
-const dotenv = require('dotenv');
 const path = require('path'); 
 const fs = require('fs');
-
+const dotenv = require('dotenv');
 const result = dotenv.config({path: path.resolve(__dirname, "../.env")});
-
-//TESTING
-const test_path = path.resolve(__dirname, "../.env")
-console.log("test_path ", test_path)
-console.log("Exists: ", fs.existsSync(test_path));
 
 if (result.error) {
   throw result.error;
 }
 
+//Exports config variables defined in ../.env
 module.exports = {
   MONGO_DB_PASSWORD : process.env.MONGO_DB_PASSWORD,
 };
