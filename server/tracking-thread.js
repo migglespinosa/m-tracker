5a1e40e83c7f56a9307ff7c05288c753ca8246a1
Miguel Espinosa <miguelespinosaiv@gmail.com>
2021-07-26 23:16:50 -0400

    Data now writes to mongoDB

--- server/tracking-thread.js
+++ server/tracking-thread.js
@@ -2,35 +2,30 @@ const {workerData, parentPort} = require('worker_threads');
 const { MongoClient } = require('mongodb');
 const { MONGO_DB_PASSWORD } = require('./config.js');
 
+//MongoDB URI
 const uri = "mongodb+srv://tracker-master:"
 const path = MONGO_DB_PASSWORD + "@cluster0.bksvx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 const client = new MongoClient(uri + path, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 //Parse data into JSON
 const message = JSON.parse(workerData);
 const session = message.body;
 
-//Iterate through batch
-session.data.forEach(elem => {
+async function write_data(){
 
-    //Send data to respective collections in MongoDB
-    if(elem.page_name.includes("Home")){
-        parentPort.postMessage(elem);
+    try{
+
+        await client.connect();
+        const db_test = client.db("db_test");
+        const collection_test = db_test.collection("collection_test");
+        const result = await collection_test.insertOne(session);
+        
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+
     }
-
-    if(elem.page_name.includes("Page A")){
-        parentPort.postMessage(elem);
+    catch(error){
+        console.log(error);
     }
+}
 
-    if(elem.page_name.includes("Page B")){
-        parentPort.postMessage(elem);
-    }
-    
-})
-
+write_data();
