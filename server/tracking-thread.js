87541f874fc7ba154227452d73f5461c68a6a48f
Miguel Espinosa <miguelespinosaiv@gmail.com>
2021-07-29 22:53:06 -0400

    Added method to only write to MongoDB if there is new session data

--- server/tracking-thread.js
+++ server/tracking-thread.js
@@ -9,20 +9,19 @@ const client = new MongoClient(uri + path, { useNewUrlParser: true, useUnifiedTo
 
 //Parse data into JSON
 const message = JSON.parse(workerData);
-const session = message.body;
-const time_stamp = session.load_time;
-const account_number = session.account_number;
+const site_session = message.body;
+const time_stamp = site_session.load_time;
+const account_number = site_session.account_number;
 
 //Send data to MongoDB
 async function write_data(){
-
 
     try{
         //Insert a document to database "db_test" in collection "collection_test"
         await client.connect();
         const db_test = client.db("db_test");
         const collection_test = db_test.collection("collection_test");
-        const result = await collection_test.insertOne(session);
+        const result = await collection_test.insertOne(site_session);
         console.log(`A document was inserted with the _id: ${result.insertedId}`);
     }
     catch(error){
@@ -30,7 +29,24 @@ async function write_data(){
     }
 }
 
-write_data();
+//Returns false if no new pages were switched and no click or hover data were appended
+function new_data(site_session){
+
+    if( (site_session.page_sessions.length == 1) &&
+        (site_session.page_sessions[0].click_data.length == 0) && 
+        (site_session.page_sessions[0].hover_data.length == 0)){
+        return false;
+    }
+    else{
+        return true;
+    }
+    
+}
+
+//Only writes to MongoDB if there is new session data
+if(new_data(site_session) == true){
+    write_data();
+}
 
 /*
 Potential algorithm for updating a session in mongodb:
@@ -46,5 +62,5 @@ Potential algorithm for updating a session in mongodb:
     and return most recent page-session in the mongoDB   
     - Check if page-sessions' name and load times match
         - If matching, append click-data and hover-data to MongoDB page-session
-        - Else, append entire rest of page-session to MongoDB
+    - Append entire rest of page-session to MongoDB
 */
