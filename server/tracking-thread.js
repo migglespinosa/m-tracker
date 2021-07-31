f01350ae5267e58c625224472d374f284aa3a655
Miguel Espinosa <miguelespinosaiv@gmail.com>
2021-07-31 16:58:21 -0400

    Implemented updating session_data code

--- server/tracking-thread.js
+++ server/tracking-thread.js
@@ -10,19 +10,51 @@ const client = new MongoClient(uri + path, { useNewUrlParser: true, useUnifiedTo
 //Parse data into JSON
 const message = JSON.parse(workerData);
 const site_session = message.body;
-const time_stamp = site_session.load_time;
+const load_time = site_session.load_time;
 const account_number = site_session.account_number;
+
+const oldest_page_session_load_time = site_session.page_sessions[0].load_time;
+const oldest_page_session_page_name = site_session.page_sessions[0].page_name;
+const oldest_page_session_hover_data = site_session.page_sessions[0].hover_data;
+const oldest_page_session_click_data = site_session.page_sessions[0].click_data;
 
 //Send data to MongoDB
 async function write_data(){
 
     try{
+
+        const site_session_query = { $and: [{load_time: {$eq: load_time}}, {account_number: {$eq: account_number}}]};
+        const site_session_projection = {page_sessions: { $slice: -1}};
+        const site_session_filter = site_session_query;
+        const site_session_update = {
+            $push: {
+                "page_sessions.$[page].click_data": oldest_page_session_click_data,
+                "page_sessions.$[page].hover_data": oldest_page_session_hover_data
+            }
+        } 
+        const options = {
+            arrayFilters: [{
+                "page.load_time" : oldest_page_session_load_time,
+                "page.page_name" : oldest_page_session_page_name
+            }]
+        };
+
         //Insert a document to database "db_test" in collection "collection_test"
         await client.connect();
         const db_test = client.db("db_test");
         const collection_test = db_test.collection("collection_test");
-        const result = await collection_test.insertOne(site_session);
-        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        const site_session_result = await collection_test.findOne(site_session_query);
+        console.log("site_session_result:", site_session_result);
+
+        if(!site_session_result){
+            const result = await collection_test.insertOne(site_session);
+            console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        }
+        else{
+            const result = await movies.updateOne(site_session_filter, site_session_update, options);
+            console.log("Result: ", result);
+        }
+
     }
     catch(error){
         console.error(error);
